7e6e0f9e0210ef3d8773043068de363f313d0dc5
Shigma <shigma10826@gmail.com>
2022-10-06 03:33:14 +0800

    feat: add config `concurrency`

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import { Context, Schema, Time } from 'koishi'
+import { Context, Dict, Logger, Schema, Time } from 'koishi'
 
 export const reactive = true
 export const name = 'novelai'
 
+const logger = new Logger('novelai')
+
 export interface Config {
   token: string
   timeout?: number
+  concurrency?: number
 }
 
 export const Config: Schema<Config> = Schema.object({
   token: Schema.string().description('API token。').required(),
   timeout: Schema.number().role('time').description('默认的请求时间。').default(Time.minute * 0.5),
+  concurrency: Schema.number().description('单个频道下的最大并发数量。'),
 })
 
 const UNDESIRED = 'nsfw, lowres, text, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
 
 export const RESOLUTIONS = {
-  '横': {height: 512, width: 768},
-  '竖': {height: 768, width: 512},
-  '方': {height: 640, width: 640}
+  '横': { height: 512, width: 768 },
+  '竖': { height: 768, width: 512 },
+  '方': { height: 640, width: 640 },
 }
 
 function assembleMsgNode(user: {uin: string, name: string}, content: string | string[] | {}) {
@@ -33,20 +37,31 @@ function assembleMsgNode(user: {uin: string, name: string}, content: string | st
 }
 
 export function apply(ctx: Context, config: Config) {
-  ctx.command('novelai <prompts:text>')
+  const states: Dict<Set<string>> = Object.create(null)
+
+  ctx.guild().command('novelai <prompts:text>')
     .shortcut('画画', { fuzzy: true })
     .shortcut('约稿', { fuzzy: true })
     .usage('使用英文 tag，用逗号隔开，例如 Mr.Quin,dark sword,red eyes，查找tag使用Danbooru')
-    .option('res', '-r <resolution:str>', {fallback: '竖'})
+    .option('res', '-r <resolution:str>', { fallback: '竖' })
     .action(async ({session, options}, input) => {
       if (!input.trim()) return session.execute('help novelai')
-      input = input.replace(/，/g, ',')
+      input = input.replace(/，/g, ', ').replace(/\s+/g, ' ')
       if (/[^\s\w,\[\]\{\}]/.test(input)) return '只能用英文输入。'
 
-      const seed = Math.round(new Date().getTime() / 1000)
-      session.send('在画了在画了')
+      const id = Math.random().toString(36).slice(2)
+      if (config.concurrency) {
+        states[session.cid] ||= new Set()
+        if (states[session.cid].size >= config.concurrency) {
+          return '请稍后再试。'
+        } else {
+          states[session.cid].add(id)
+        }
+      }
 
       try {
+        const seed = Math.round(new Date().getTime() / 1000)
+        session.send('在画了在画了')
         const resolution = RESOLUTIONS[options.res] || RESOLUTIONS['横']
         const art = await ctx.http.axios('https://api.novelai.net/ai/generate-image', {
           method: 'POST',
@@ -93,8 +108,10 @@ export function apply(ctx: Context, config: Config) {
           session.channelId, [infoNode, artNode]
         )
       } catch(err) {
-        console.error(err)
-        return '发生错误'
+        logger.error(err)
+        return '发生错误。'
+      } finally {
+        states[session.cid].delete(id)
       }
     })
 }
