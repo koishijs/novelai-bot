6e5fdf5f062ceb93d1a397ff68ee26e98eb7e0e4
Ninzore <33434617+Ninzore@users.noreply.github.com>
2022-10-07 02:33:05 +0100

    fix: add return types

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import sizeOf from 'image-size'
+import { ISizeCalculationResult } from 'image-size/dist/types/interface'
 
 const MAX_CONTENT_SIZE = 10485760
 const ALLOW_MIMETYPE = ['jpeg', 'png']
 
-async function readRemote(url: string, headers: {}) {
+async function readRemote(url: string, headers: {}): Promise<Buffer> {
     const head = await axios.head(url, { headers })
     
     if (parseInt(head.headers['content-length']) > MAX_CONTENT_SIZE) throw 'file too large'
@@ -18,7 +19,7 @@ async function readRemote(url: string, headers: {}) {
     }).then(res => { return res.data })
 }
 
-function getImgSize(img: Buffer) {
+function getImgSize(img: Buffer): ISizeCalculationResult {
     return sizeOf(img)
 }
 
