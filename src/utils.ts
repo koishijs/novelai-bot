61f79fa951a6c401f742072eb71e7e55e3aa90a3
Ninzore <33434617+Ninzore@users.noreply.github.com>
2022-10-10 02:01:50 +0100

    feat: calculate hash for login (UNFINISHED)

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { Context } from 'koishi'
+import { argon2id, blake2b } from 'hash-wasm'
 
 const MAX_CONTENT_SIZE = 10485760
 const ALLOWED_TYPES = ['jpeg', 'png']
@@ -15,4 +16,23 @@ export async function download(ctx: Context, url: string, headers = {}): Promise
   }
 
   return ctx.http.get(url, { responseType: 'arraybuffer', headers })
+}
+
+export async function calcHash(username: string, password: string): Promise<string> {
+  const salt = await blake2b(
+    password.substring(0, 6) + username + 'novelai_data_access_key',
+    16 * 8,
+    null,
+  )
+
+  /* TODO: 这地方结果是错的需要改 */
+  return argon2id({
+    hashLength: 64,
+    password,
+    salt,
+    iterations: 1,
+    parallelism: 1,
+    memorySize: 2000000,
+    outputType: 'hex',
+  }).then(res => { return Buffer.from(res, 'hex').toString('base64url') })
 }
