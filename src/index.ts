6010aec78682c684e1b359a9415da95b93bb2e27
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2022-11-09 14:41:00 +0800

    fix: sd-webui reporting 500 error using img2img (#108)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Context, Dict, Logger, omit, Quester, segment, Session, trimSlash } from 'koishi'
 import { Config, modelMap, models, orientMap, parseForbidden, parseInput, sampler } from './config'
-import { StableDiffusionWebUI } from './types'
+import { ImageData, StableDiffusionWebUI } from './types'
 import { closestMultiple, download, getImageSize, login, NetworkError, project, resizeInput, Size, stripDataPrefix } from './utils'
 import {} from '@koishijs/plugin-help'
 
@@ -103,7 +103,7 @@ export function apply(ctx: Context, config: Config) {
     .action(async ({ session, options }, input) => {
       if (!input?.trim()) return session.execute('help novelai')
 
-      let imgUrl: string
+      let imgUrl: string, image: ImageData
       if (!restricted(session)) {
         input = segment.transform(input, {
           image(attrs) {
@@ -154,7 +154,6 @@ export function apply(ctx: Context, config: Config) {
       }
 
       if (imgUrl) {
-        let image: [ArrayBuffer, string]
         try {
           image = await download(ctx, imgUrl)
         } catch (err) {
@@ -166,12 +165,11 @@ export function apply(ctx: Context, config: Config) {
         }
 
         Object.assign(parameters, {
-          image: image[1],
           scale: options.scale ?? 11,
           steps: options.steps ?? 50,
         })
         if (options.enhance) {
-          const size = getImageSize(image[0])
+          const size = getImageSize(image.buffer)
           if (size.width + size.height !== 1280) {
             return session.text('.invalid-size')
           }
@@ -182,7 +180,7 @@ export function apply(ctx: Context, config: Config) {
             strength: options.strength ?? 0.2,
           })
         } else {
-          options.resolution ||= resizeInput(getImageSize(image[0]))
+          options.resolution ||= resizeInput(getImageSize(image.buffer))
           Object.assign(parameters, {
             height: options.resolution.height,
             width: options.resolution.width,
@@ -223,7 +221,7 @@ export function apply(ctx: Context, config: Config) {
       const path = (() => {
         switch (config.type) {
           case 'sd-webui':
-            return parameters.image ? '/sdapi/v1/img2img' : '/sdapi/v1/txt2img'
+            return image ? '/sdapi/v1/img2img' : '/sdapi/v1/txt2img'
           case 'naifu':
             return '/generate-stream'
           default:
@@ -234,13 +232,14 @@ export function apply(ctx: Context, config: Config) {
       const data = (() => {
         if (config.type !== 'sd-webui') {
           parameters.sampler = sampler.sd2nai(options.sampler)
+          parameters.image = image.base64 // NovelAI / NAIFU accepts bare base64 encoded image
           if (config.type === 'naifu') return parameters
           return { model, input: prompt, parameters: omit(parameters, ['prompt']) }
         }
 
         return {
           sampler_index: sampler.sd[options.sampler],
-          init_images: parameters.image && [parameters.image],
+          init_images: image && [image.dataUrl], // sd-webui accepts data URLs with base64 encoded image
           ...project(parameters, {
             prompt: 'prompt',
             batch_size: 'n_samples',
