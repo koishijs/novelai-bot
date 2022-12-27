78daf1de80ed932fe7f8383c3a08acd100bc525b
Shigma <shigma10826@gmail.com>
2022-12-27 16:24:13 +0800

    feat: migrate base64 to data URL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Context, Dict, Logger, omit, Quester, segment, Session, trimSlash } from 'koishi'
 import { Config, modelMap, models, orientMap, parseForbidden, parseInput, sampler, upscalers } from './config'
 import { ImageData, StableDiffusionWebUI } from './types'
-import { closestMultiple, download, getImageSize, login, NetworkError, project, resizeInput, Size, stripDataPrefix } from './utils'
+import { closestMultiple, download, forceDataPrefix, getImageSize, login, NetworkError, project, resizeInput, Size } from './utils'
 import {} from '@koishijs/translator'
 import {} from '@koishijs/plugin-help'
 
@@ -289,18 +289,18 @@ export function apply(ctx: Context, config: Config) {
           data: getPayload(),
         }).then((res) => {
           if (config.type === 'sd-webui') {
-            return stripDataPrefix((res.data as StableDiffusionWebUI.Response).images[0])
+            return forceDataPrefix((res.data as StableDiffusionWebUI.Response).images[0])
           }
           // event: newImage
           // id: 1
           // data:
-          return res.data?.slice(27)
+          return forceDataPrefix(res.data?.slice(27))
         })
 
-        let base64: string, count = 0
+        let dataUrl: string, count = 0
         while (true) {
           try {
-            base64 = await request()
+            dataUrl = await request()
             break
           } catch (err) {
             if (Quester.isAxiosError(err)) {
@@ -313,10 +313,10 @@ export function apply(ctx: Context, config: Config) {
           }
         }
 
-        if (!base64.trim()) return await session.send(session.text('.empty-response'))
+        if (!dataUrl.trim()) return await session.send(session.text('.empty-response'))
 
         function getContent() {
-          if (options.output === 'minimal') return segment.image('base64://' + base64)
+          if (options.output === 'minimal') return segment.image(dataUrl)
           const attrs = {
             userId: session.userId,
             nickname: session.author?.nickname || session.username,
@@ -344,7 +344,7 @@ export function apply(ctx: Context, config: Config) {
           if (options.output === 'verbose') {
             result.children.push(segment('message', attrs, `undesired = ${uc}`))
           }
-          result.children.push(segment('message', attrs, segment.image('base64://' + base64)))
+          result.children.push(segment('message', attrs, segment.image(dataUrl)))
           return result
         }
 
@@ -409,7 +409,7 @@ export function apply(ctx: Context, config: Config) {
         return session.text('.download-error')
       }
 
-      const data: StableDiffusionWebUI.ExtraSingleImageRequest = {
+      const payload: StableDiffusionWebUI.ExtraSingleImageRequest = {
         image: image.dataUrl,
         resize_mode: options.resolution ? 1 : 0,
         show_extras_results: true,
@@ -424,16 +424,15 @@ export function apply(ctx: Context, config: Config) {
       }
 
       try {
-        const resp = await ctx.http.axios(trimSlash(config.endpoint) + '/sdapi/v1/extra-single-image', {
+        const { data } = await ctx.http.axios<StableDiffusionWebUI.ExtraSingleImageResponse>(trimSlash(config.endpoint) + '/sdapi/v1/extra-single-image', {
           method: 'POST',
           timeout: config.requestTimeout,
           headers: {
             ...config.headers,
           },
-          data,
+          data: payload,
         })
-        const base64 = stripDataPrefix((resp.data as StableDiffusionWebUI.ExtraSingleImageResponse).image)
-        return segment.image('base64://' + base64)
+        return segment.image(forceDataPrefix(data.image))
       } catch (e) {
         logger.warn(e)
         return session.text('.unknown-error')
