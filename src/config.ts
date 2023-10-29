d714e6ba5e9026dff964dcedb72dca585cb959f6
student_2333 <lgc2333@126.com>
2023-10-30 00:10:15 +0800

    refactor

--- src/config.ts
+++ src/config.ts
@@ -420,13 +420,12 @@ export function parseInput(session: Session, input: string, config: Config, over
 
   const negative = []
   const placement = session.resolve(config.placement)
-  const lowercase = session.resolve(config.lowercase)
   const appendToList = (words: string[], input = '') => {
     const tags = input.split(/,\s*/g)
     if (placement === 'before') tags.reverse()
     for (let tag of tags) {
       tag = tag.trim()
-      if (lowercase) tag = tag.toLowerCase()
+      if (config.lowercase) tag = tag.toLowerCase()
       if (!tag || words.includes(tag)) continue
       if (placement === 'before') {
         words.unshift(tag)
